2bd711df77bc287c1f1451db3edc9984497be5b3
xxf098 <xxf098@gmail.com>
2019-07-09 02:52:30 -0400

    add connection

--- shadowsocks/ssrforward.py
+++ shadowsocks/ssrforward.py
@@ -1,8 +1,13 @@
 import asyncio
 import logging
+import sys
 from collections import namedtuple
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('ssrforward')
 CRLF, COLON, SP = b'\r\n', b':', b' '
+PY3 = sys.version_info[0] == 3
+text_type = str
+binary_type = bytes
+from urllib import parse as urlparse
 
 default_buf_size=8192
 def client_connection(reader, writer):
@@ -10,13 +15,77 @@ def client_connection(reader, writer):
 
 async def client_task(reader, writer):
     data = await reader.read(default_buf_size)
-    negotiate_http(data)
+    proxy = Proxy()
+    proxy.negotiate_http(data)
 
-def negotiate_http(data):
-    request = HttpParser(HttpParser.types.REQUEST_PARSER)
-    request.parse(data)
-    if request.state == HttpParser.states.COMPLETE:
-        pass
+class Proxy(object):
+
+    def __init__(self):
+        self.sock5_addr = ('127.0.0.1', 8088)
+        self.request = HttpParser(HttpParser.types.REQUEST_PARSER)
+        self.response = HttpParser(HttpParser.types.RESPONSE_PARSER)
+    
+    def negotiate_http(self, data):
+        self.request.parse(data)
+        if self.request.state == HttpParser.states.COMPLETE:
+            if self.request.method == b'CONNECT':
+                host, port = request.url.path.split(COLON)
+            elif request.url:
+                host, port = request.url.hostname, request.url.port if request.url.port else 80
+            else:
+                raise Exception('Invalid request\n%s' % request.raw)        
+
+# send receive data
+class Connection(object):
+    """TCP server/client connection abstraction."""
+
+    def __init__(self, what):
+        self.reader = None
+        self.writer = None
+        self.buffer = b''
+        self.closed = False
+        self.what = what  # server or client
+
+    async def send(self, data):
+        # TODO: Gracefully handle BrokenPipeError exceptions
+        self.writer.write(data)
+        await self.writer.drain()
+        return len(data)
+
+    async def recv(self, bufsiz=8192):
+        try:
+            data = await self.reader.read(bufsiz)
+            if len(data) == 0:
+                logger.debug('rcvd 0 bytes from %s' % self.what)
+                return None
+            logger.debug('rcvd %d bytes from %s' % (len(data), self.what))
+            return data
+        except Exception as e:
+            if e.errno == errno.ECONNRESET:
+                logger.debug('%r' % e)
+            else:
+                logger.exception(
+                    'Exception while receiving from connection %s %r with reason %r' % (self.what, self.conn, e))
+            return None
+
+    async def close(self):
+        self.writer.close()
+        await writer.wait_closed()
+        self.closed = True
+
+    def buffer_size(self):
+        return len(self.buffer)
+
+    def has_buffer(self):
+        return self.buffer_size() > 0
+
+    def queue(self, data):
+        self.buffer += data
+
+    def flush(self):
+        sent = self.send(self.buffer)
+        self.buffer = self.buffer[sent:]
+        logger.debug('flushed %d bytes to %s' % (sent, self.what))
 
 class ChunkParser(object):
 
@@ -127,10 +196,86 @@ class HttpParser(object):
 
         return len(data) > 0, data    
 
+    def process_line(self, data):
+        line = data.split(SP)
+        if self.type == HttpParser.types.REQUEST_PARSER:
+            self.method = line[0].upper()
+            self.url = urlparse.urlsplit(line[1])
+            self.version = line[2]
+        else:
+            self.version = line[0]
+            self.code = line[1]
+            self.reason = b' '.join(line[2:])
+        self.state = HttpParser.states.LINE_RCVD
+
+    # Proxy-Connection: keep-alive
+    def process_header(self, data):
+        if len(data) == 0:
+            if self.state == HttpParser.states.RCVING_HEADERS:
+                self.state = HttpParser.states.HEADERS_COMPLETE
+            elif self.state == HttpParser.states.LINE_RCVD:
+                self.state = HttpParser.states.RCVING_HEADERS
+        else:
+            self.state = HttpParser.states.RCVING_HEADERS
+            parts = data.split(COLON)
+            key = parts[0].strip()
+            value = COLON.join(parts[1:]).strip()
+            self.headers[key.lower()] = (key, value)
+
+    def build_url(self):
+        if not self.url:
+            return b'/None'
+
+        url = self.url.path
+        if url == b'':
+            url = b'/'
+        if not self.url.query == b'':
+            url += b'?' + self.url.query
+        if not self.url.fragment == b'':
+            url += b'#' + self.url.fragment
+        return url
+
+    def build(self, del_headers=None, add_headers=None):
+        req = b' '.join([self.method, self.build_url(), self.version])
+        req += CRLF
+
+        if not del_headers:
+            del_headers = []
+        for k in self.headers:
+            if k not in del_headers:
+                req += self.build_header(self.headers[k][0], self.headers[k][1]) + CRLF
+
+        if not add_headers:
+            add_headers = []
+        for k in add_headers:
+            req += self.build_header(k[0], k[1]) + CRLF
+
+        req += CRLF
+        if self.body:
+            req += self.body
+
+        return req
+
+    @staticmethod
+    def build_header(k, v):
+        return k + b': ' + v
+
+    @staticmethod
+    def split(data):
+        pos = data.find(CRLF)
+        if pos == -1:
+            return False, data
+        line = data[:pos]
+        data = data[pos + len(CRLF):]
+        return line, data
+
 #TODO: polipo
+#TODO: authcode
 def main():
     host, port = '127.0.0.1', 9050
-    logger.info('Serving on {}:{}'.format(host, port))
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(funcName)s:%(lineno)d - %(message)s')
+    logger.info(f'Serving on {host}:{port}')
     loop = asyncio.get_event_loop()
     future = asyncio.start_server(client_connection,
                 host=host,
